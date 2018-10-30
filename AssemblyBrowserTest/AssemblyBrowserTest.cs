658dce4b3855e55102400272192fe88e44fdc543
Vladislav Frolov <37739674+nnFrolovnn@users.noreply.github.com>
2018-10-30 14:48:07 +0300

    create tests

---
+++ AssemblyBrowserTest/AssemblyBrowserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using AssemblyBrowserLib;
+using AssemblyBrowserLib.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssemblyBrowserTest
+{
+    [TestClass]
+    public class AssemblyBrowserTest
+    {
+        AssemblyReader reader;
+        List<Namespace> namespaces;
+
+        [TestInitialize]
+        private void Init()
+        {
+            const string path = @"D:\учеба\5 семестр\СПП\Faker\Faker\bin\Debug\Faker.dll";
+            reader = new AssemblyReader();
+            namespaces = reader.GetAssembly(path);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+                Assert.IsNotNull(namespaces);
+        }
+    }
+}
---
+++ AssemblyBrowserTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyBrowserTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyBrowserTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35471808-b040-4442-9480-f3ebdf5607c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
